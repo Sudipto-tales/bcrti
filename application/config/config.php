7e6d8db04060638aa23c3fcbc39d9d83cb725c49
Sudipta Ghosh <152256678+Sudipto-tales@users.noreply.github.com>
2024-09-12 14:21:48 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set('Asia/Kolkata');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/bcrti/';
+$config['base_url'] = 'https://sudipto-tales.github.io/bcrti/';
 
 /*
 |--------------------------------------------------------------------------
